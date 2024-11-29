cca776b0b2af626933be87e0cfe115b82f082e6d
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-29 11:32:09 +0100

    header modification for kwargs in CookieRequester.py

--- API/CookieRequester.py
+++ API/CookieRequester.py
@@ -10,16 +10,40 @@ class CookieRequester(AbstractRequester):
         self.headers.update({'Cookie': f'acm-awv={cookie}'})
 
     def get(self, url: str = '', **kwargs) -> Response:
+        kwargs = self.modify_kwargs_for_bearer_token(kwargs)
         return super().get(url=url, **kwargs)
 
     def post(self, url: str = '', **kwargs) -> Response:
+        kwargs = self.modify_kwargs_for_bearer_token(kwargs)
         return super().post(url=url, **kwargs)
 
     def put(self, url: str = '', **kwargs) -> Response:
+        kwargs = self.modify_kwargs_for_bearer_token(kwargs)
         return super().put(url=url, **kwargs)
 
     def patch(self, url: str = '', **kwargs) -> Response:
+        kwargs = self.modify_kwargs_for_bearer_token(kwargs)
         return super().patch(url=url, **kwargs)
 
     def delete(self, url: str = '', **kwargs) -> Response:
+        kwargs = self.modify_kwargs_for_bearer_token(kwargs)
         return super().delete(url=url, **kwargs)
+
+    @staticmethod
+    def modify_kwargs_for_bearer_token(kwargs: dict) -> dict:
+        if 'headers' not in kwargs:
+            kwargs['headers'] = {}
+
+        for arg, headers in kwargs.items():
+            if arg == 'headers':
+                if 'accept' not in headers:
+                    headers['accept'] = ''
+                if headers["accept"] is not None:
+                    headers["accept"] = (
+                        headers["accept"] + ", application/json"
+                        if headers["accept"] != ''
+                        else "application/json"
+                    )
+                headers['Content-Type'] = 'application/vnd.awv.eminfra.v1+json'
+                kwargs['headers'] = headers
+        return kwargs
