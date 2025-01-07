285e428cbc27bf749ed98f02ab27cbed341990ed
VerdoodtDries <dries@nordend.eu>
2025-01-07 10:32:27 +0100

    Completed docstring

--- Generic/ExcelModifier.py
+++ Generic/ExcelModifier.py
@@ -4,6 +4,7 @@ from openpyxl import load_workbook
 from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
 
+from API.EMInfraDomain import ApplicationEnum
 from API.Enums import Environment
 
 
@@ -23,38 +24,39 @@ class ExcelModifier:
             Environment.AIM: 'https://services-aim.apps-dev.mow.vlaanderen.be/awvinfra/ui/?asset='
         }
 
-    def add_hyperlink(self, sheet: str = None, link_type: str = 'eminfra',
+    def add_hyperlink(self, sheet: str = None, link_type: ApplicationEnum = ApplicationEnum.EM_INFRA,
                                 env: Environment = Environment.PRD) -> None:
-        """Convert uuid in Excel file to a formula
-    
-        Assumption that the column is called uuid (case-insensitive).
+        """Adds a hyperlink to the Excel file
 
-        # TODO docstring aanvullen
-        # TODO Ander formaat docstring toepassen: reStructuredText
-        :param sheet: str
-            Name of the Excel sheet.
-        :param link_type:
-            eminfra or elisa
-        :param env: Environment enumeration
+        Adds a hyperlink to the column named "uuid", linking to the applications eminfra or elisainfra.
+        Cell is formatted (underline and text colour=blue)
+
+        :param sheet: Excel sheet name
+        :type sheet: str
+        :param link_type: link to AWV application
+        :type link_type: ApplicationEnum
+        :param env: environment
+        :type env: Environment
         :return: None
         """
         wb = load_workbook(filename=self.file_path)
         ws = wb[f"{sheet}"] if sheet else wb.active  # read active sheet when empty
 
-        # Find the column index for the column named "uuid"
-        column_letter = None
-        for col_idx, cell in enumerate(ws[1], start=1):  # Iterate through the first row (header)
-            if str.lower(cell.value) == "uuid":  # Check if the column name matches "uuid"
-                column_letter = get_column_letter(col_idx)
-                break  # break out of the for-loop, once the column is found.
+        column_letter = next(
+            (
+                get_column_letter(col_idx)
+                for col_idx, cell in enumerate(ws[1], start=1)
+                if str.lower(cell.value) == "uuid"
+            ),
+            None,
+        )
 
-        # Write a formula in column "C" to sum columns "A" and "B"
         for row in range(2, ws.max_row + 1):  # Data starts from row 2 in Excel
             asset_uuid = ws[f"{column_letter}{row}"].value
-            if link_type == 'eminfra':
-                url_prefix = self.first_part_url_dict_eminfra[env]
-            elif link_type == 'elisainfra':
+            if link_type == ApplicationEnum.ELISA_INFRA:
                 url_prefix = self.first_part_url_dict_elisainfra[env]
+            elif link_type == ApplicationEnum.EM_INFRA:
+                url_prefix = self.first_part_url_dict_eminfra[env]
             else:
                 raise ValueError("Value must be one of {'eminfra', 'elisainfra'}")
             # install a link
