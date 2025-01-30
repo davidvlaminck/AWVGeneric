37944f19c3a12acd55735bc58008b955995ac2c3
VerdoodtDries <dries@nordend.eu>
2025-01-30 16:04:53 +0100

    converts str to datetime for all dates. Removed parse_date() function.

--- utils/date_helpers.py
+++ utils/date_helpers.py
@@ -3,20 +3,17 @@ from datetime import timedelta, datetime
 import pytz
 
 
-def get_winter_summer_time_interval(date_str, timezone='Europe/Brussels') -> int:
+def get_winter_summer_time_interval(date: datetime, timezone='Europe/Brussels') -> int:
     """
-    Determines if the given date is in summer time (DST) or winter time (Standard Time).
+    Determines if the given datetime is in summer time (DST) or winter time (Standard Time).
 
-    :param date_str: The date as a string (e.g., "2024-06-15")
+    :param date: datetime
     :param timezone: The time zone to check (default: "Europe/Brussels")
     :return: 2 during "Summer Time (DST)" and 1 during "Winter Time (Standard Time)"
     """
-    # Convert string date to datetime object
-    dt = datetime.strptime(date_str, '%Y-%m-%d')
-
     # Assign timezone information
     tz = pytz.timezone(timezone)
-    dt_tz = tz.localize(dt)
+    dt_tz = tz.localize(date)
 
     # Check daylight saving time (DST)
     if dt_tz.dst() != timedelta(0):
@@ -25,50 +22,30 @@ def get_winter_summer_time_interval(date_str, timezone='Europe/Brussels') -> int
         return 1  # "Winter Time (Standard Time)"
 
 
-def validate_dates(start_date: str = None, end_date: str = None):
+def validate_dates(start_datetime: datetime = None, end_datetime: datetime = None) -> bool:
     """
     Validates that at least one date is provided, converts string dates to datetime,
     and ensures that start_date (if given) is earlier than end_date.
 
-    :param start_date: The start date as a string (optional)
-    :param end_date: The end date as a string (optional)
-    :return: A tuple (start_date, end_date) as datetime objects
+    :param start_datetime: start date
+    :param end_datetime: end date
+    :return: True on successful validation
     :raises ValueError: If validation fails
     """
-    # Ensure at least one date is provided
-    if start_date is None and end_date is None:
-        raise ValueError("At least one of start_date or end_date must be provided.")
-
-    # Convert string dates to datetime objects (or None if empty)
-    start_date_parsed = parse_date(date_str=start_date)
-    end_date_parsed = parse_date(date_str=end_date)
-
-    # Check if start_date is earlier than end_date (if both are provided)
-    if start_date_parsed and end_date_parsed and start_date_parsed >= end_date_parsed:
-        raise ValueError("start_date and end_date must be in chronical order")
-
-    return start_date, end_date
+    # One of both parameters must be provided
+    if start_datetime is None and end_datetime is None:
+        raise ValueError("One of both parameters 'start_datetime' or 'end_datetime' must be provided")
+    # Check if both are present and if the start_date is earlier than end_date
+    elif start_datetime and end_datetime and start_datetime >= end_datetime:
+        raise ValueError("start_datetime and end_datetime must be in chronical order")
+    return True
 
 
-def parse_date(date_str: str) -> datetime:
-    """Parse date from String to Datetime format
+def format_datetime(datetime: datetime) -> str:
+    """ Formats datetime to a string, including the correct time_interval during winter/summer time
 
-    :param date_str: date in string format, structure '%Y-%m-%d'
-    :return: date in datetime format
+    :param datetime: datetime
+    :return: date as string '%Y-%m-%dT00:00:00.000+00:00'
     """
-    try:
-        return datetime.strptime(date_str, '%Y-%m-%d') if date_str else None
-    except ValueError as e:
-        raise ValueError(
-            'Invalid date format. Expected format: %Y-%m-%d'
-        ) from e
-
-
-def format_date(date_str: str) -> str:
-    """ Formats date to a complete date, including the correct time_interval during winter/summer time
-
-    :param date_str: '%Y-%m-%d'
-    :return: date as string '%Y-%m-%d'
-    """
-    hour_interval = get_winter_summer_time_interval(date_str=date_str)
-    return f'{date_str}T00:00:00.000+0{hour_interval}:00'
+    hour_interval = get_winter_summer_time_interval(date=datetime)
+    return f'{datetime.strftime("%Y-%m-%d")}T00:00:00.000+0{hour_interval}:00'
