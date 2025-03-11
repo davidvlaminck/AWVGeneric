62990008b867aeb4d882b1a5e6749bdc33b4e839
VerdoodtDries <dries@nordend.eu>
2025-03-11 10:42:38 +0100

    Append function search_eigenschappen to retreive the eigenschap_uuid searching for an eigenschap-naam

--- UseCases/UpdateEigenschap/main.py
+++ UseCases/UpdateEigenschap/main.py
@@ -7,10 +7,9 @@ if __name__ == '__main__':
     from pathlib import Path
 
     settings_path = Path().home() / 'OneDrive - Nordend/projects/AWV/resources/settings_SyncOTLDataToLegacy.json'
-    eminfra_client = EMInfraClient(env=Environment.PRD, auth_type=AuthType.JWT, settings_path=settings_path)
-    # Authenticatie methode JWT werkt niet met de Nordend-token > gebruik de cookie
-    # emson_client = EMSONClient(auth_type=AuthType.JWT, env=Environment.PRD, settings_path=settings_path)
-    emson_client = EMSONClient(auth_type=AuthType.COOKIE, env=Environment.PRD, cookie='')
+    eminfra_client = EMInfraClient(env=Environment.TEI, auth_type=AuthType.JWT, settings_path=settings_path)
+    daily_cookie = '' # to complete
+    emson_client = EMSONClient(auth_type=AuthType.COOKIE, env=Environment.TEI, cookie=daily_cookie)
 
     #################################################################################
     ####  Read RSA-report as input
@@ -24,10 +23,10 @@ if __name__ == '__main__':
         asset_uuid = asset['uuid']
         print(f"Updating eigenschap 'serienummer' for asset: {asset_uuid}")
 
-        #################################################################################
-        ####  Get eigenschap
-        #################################################################################
-        # Eigenschap ophalen via Excel of via API-call (EMSON)
+        ################################################################################
+        ###  Get eigenschap
+        ################################################################################
+        # Eigenschap ophalen via API-call (EMSON)
         response = emson_client.get_asset_by_uuid(uuid=asset_uuid)
         serienummer_actual = response['lgc:SegC.serienummerSegmentController']
 
@@ -35,7 +34,15 @@ if __name__ == '__main__':
         serienummer_new = serienummer_actual.upper()
 
         #################################################################################
+        ####  Get eigenschap_uuid
+        #################################################################################
+        eigenschap_generator = eminfra_client.search_eigenschappen(eigenschap_naam='serienummer segment controller')
+        if len(eigenschap_generator) != 1:
+            raise ValueError('Multiple eigenschappen returned; Cannot extract the value of one single eigenschap')
+        else:
+            eigenschap_uuid = eigenschap_generator[0].uuid
+
+        #################################################################################
         ####  Update eigenschap
         #################################################################################
-        eigenschap_uuid = 'ce1d97ff-40bb-47b3-ac27-b491c9c52e71'  ## serienummer
         eminfra_client.update_eigenschap(asset_uuid=asset_uuid, eigenschap_uuid=eigenschap_uuid, eigenschap_waarde=serienummer_new)
