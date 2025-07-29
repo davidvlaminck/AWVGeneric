f27fee9105e3d7cbb1130c0cc801f9eaee16f435
VerdoodtDries <dries@nordend.eu>
2025-07-29 16:53:38 +0200

    Add functie zoek_puntlocatie_via_wegsegment()

--- API/Locatieservices2Client.py
+++ API/Locatieservices2Client.py
@@ -1,28 +1,14 @@
-import json
 import logging
-import uuid
 
-from collections.abc import Generator
 from dataclasses import dataclass
-from datetime import datetime, timedelta
 
 from pathlib import Path
-from datetime import datetime
 
-from API.EMInfraDomain import OperatorEnum, TermDTO, ExpressionDTO, SelectionDTO, PagingModeEnum, QueryDTO, BestekRef, \
-    BestekKoppeling, FeedPage, AssettypeDTO, AssettypeDTOList, DTOList, AssetDTO, BetrokkenerelatieDTO, AgentDTO, \
-    PostitDTO, LogicalOpEnum, BestekCategorieEnum, BestekKoppelingStatusEnum, AssetDocumentDTO, LocatieKenmerk, \
-    LogicalOpEnum, ToezichterKenmerk, IdentiteitKenmerk, AssetTypeKenmerkTypeDTO, KenmerkTypeDTO, \
-    AssetTypeKenmerkTypeAddDTO, ResourceRefDTO, Eigenschap, Event, EventType, ObjectType, EventContext, ExpansionsDTO, \
-    RelatieTypeDTO, KenmerkType, EigenschapValueDTO, RelatieTypeDTOList, BeheerobjectDTO, ToezichtgroepTypeEnum, \
-    ToezichtgroepDTO, BaseDataclass, BeheerobjectTypeDTO, BoomstructuurAssetTypeEnum, KenmerkTypeEnum, AssetDTOToestand, \
-    EigenschapValueUpdateDTO, GeometryNiveau, GeometryBron, GeometryNauwkeurigheid, GeometrieKenmerk
+from API.EMInfraDomain import BaseDataclass
 from API.Enums import AuthType, Environment
 from API.Locatieservices2Domain import WegsegmentPuntLocatie
 from API.RequesterFactory import RequesterFactory
-from utils.date_helpers import validate_dates, format_datetime
-from utils.query_dto_helpers import add_expression
-import os
+
 
 
 @dataclass
@@ -53,4 +39,14 @@ class Locatieservices2Client:
             logging.error(response)
             raise ProcessLookupError(response.content.decode("utf-8"))
 
-        return [WegsegmentPuntLocatie.from_dict(item) for item in response.json()['data']]
+        return WegsegmentPuntLocatie.from_dict(response.json())
+
+    def zoek_puntlocatie_via_wegsegment(self, ident8: str, opschrift: float, afstand: float = 0.0) -> WegsegmentPuntLocatie:
+        response = self.requester.get(
+            url=f'rest/puntlocatie/op/weg/{ident8}/via/opschrift?opschrift={opschrift}&afstand={afstand}'
+        )
+        if response.status_code != 200:
+            logging.error(response)
+            raise ProcessLookupError(response.content.decode("utf-8"))
+
+        return WegsegmentPuntLocatie.from_dict(response.json())
