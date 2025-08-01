9ffaf1dfa1a7d204f97862568a2e6be541f7e365
VerdoodtDries <dries@nordend.eu>
2025-08-01 17:35:13 +0200

    work in progress

--- UseCases/Locatieservices2/test_locatieservices2.py
+++ UseCases/Locatieservices2/test_locatieservices2.py
@@ -40,9 +40,9 @@ def parse_lichtmast_naam(naam: str):
     positie_rijweg = match.group()
 
     ident8_raw = lichtmast_naam_basis[:index]
-    opschrift = lichtmast_naam_basis[index:]
+    opschrift = str(round(float(lichtmast_naam_basis[index+1:]), 1)) # afronden tot op 1 decimaal getal
 
-    # to implement: functie om ident8_raw naar een ident8 van exact 8 digits om te zetten?
+
     ident8 = convert_ident8(ident8=ident8_raw, direction=positie_rijweg)
 
     return positie_rijweg, ident8, opschrift
@@ -69,23 +69,60 @@ if __name__ == '__main__':
     # logging.debug(f'Gevonden Puntlocatie: {wegsegment_puntlocatie}')
 
     # Import Excel-file as pandas dataframe
-    filepath_excel = Path().home() / 'Downloads' / 'Lichtmast' / 'DA-2025-43571_export.xlsx'
+    filepath_excel_input = Path().home() / 'Downloads' / 'Lichtmast' / 'DA-2025-43571_export.xlsx'
+    filepath_excel_output = Path().home() / 'Downloads' / 'Lichtmast' / 'DA-2025-XXXXX_import.xlsx'
     usecols = ['typeURI', 'assetId.identificator', 'naam', 'naampad', 'toestand', 'geometry']
-    df_assets = pd.read_excel(filepath_excel, sheet_name='Lichtmast', usecols=usecols)
+    df_assets = pd.read_excel(filepath_excel_input, sheet_name='Lichtmast', usecols=usecols)
 
+    geometrie_refentiepunt = []
+    afstand = []
     for idx, df_asset in df_assets.iterrows():
         logging.debug(f'{idx}: Processing asset: {df_asset.get("assetId.identificator")[:36]}; naam: {df_asset.get("naam")}')
         asset = eminfra_client.get_asset_by_id(assettype_id=df_asset.get('assetId.identificator')[:36])
         logging.debug(f'{idx}: Processing asset: {asset.uuid}; naam: {asset.naam}')
+        if asset.naam is None:
+            geometrie_refentiepunt.append(None)
+            afstand.append(None)
+            continue
 
         if not is_full_match(name=asset.naam, pattern='[a-zA-Z]{1}\d{1,3}[NPMXnpmx]{1}\d+\.?\d*\.[P]\d*'):
             logging.debug(f'{asset.type.label}: {asset.naam} volgt NIET de naamconventie')
+            geometrie_refentiepunt.append(None)
+            afstand.append(None)
             continue
 
-        positie_rijweg, ident8_raw, opschrift = parse_lichtmast_naam(naam=asset.naam)
+        positie_rijweg, ident8, opschrift = parse_lichtmast_naam(naam=asset.naam)
 
-        wegsegment_puntlocatie = ls2_client.zoek_puntlocatie_via_wegsegment(ident8=ident8_raw, opschrift=opschrift)
-        wkt_geom_projected = coordinates_2_wkt(wegsegment_puntlocatie.geometry.coordinates)
-        distance = get_euclidean_distance_wkt(wkt1=df_asset.get("geometry"), wkt2=wkt_geom_projected)
+        logging.debug('Zoek puntlocatie via wegsegment.')
+        logging.debug(f'ident8: {ident8}')
+        logging.debug(f'opschrift: {opschrift}')
 
-        # geometry_referentiepunt
+        try:
+            wegsegment_puntlocatie = ls2_client.zoek_puntlocatie_via_wegsegment(ident8=ident8, opschrift=opschrift)
+            wkt_geom_referentiepunt = coordinates_2_wkt(wegsegment_puntlocatie.geometry.coordinates)
+
+            wkt1 = df_asset.get("geometry")
+            wkt2 = wkt_geom_referentiepunt
+            if pd.isna(wkt1) or pd.isna(wkt2):
+                distance = None
+            else:
+                distance = round(get_euclidean_distance_wkt(wkt1=df_asset.get("geometry"), wkt2=wkt_geom_referentiepunt))
+
+        except:
+            logging.debug('Locatieservices kon de locatie niet ophalen.')
+            wkt_geom_referentiepunt = 'werd niet teruggevonden'
+            distance = None
+
+        geometrie_refentiepunt.append(wkt_geom_referentiepunt)
+        afstand.append(distance)
+
+
+    # Append to dataframe
+    df_assets["geometrie_referentiepunt"] = geometrie_refentiepunt
+    df_assets["afstand"] = afstand
+
+    # Append hyperlink
+    df_assets["eminfra"] = 'https://apps.mow.vlaanderen.be/eminfra/assets/' + df_assets["assetId.identificator"][:36]
+
+    logging.debug(f'Write pandas dataframe to Excel: {filepath_excel_output}')
+    df_assets.to_excel(filepath_excel_output, sheet_name='Lichtmast', freeze_panes=[1,2], index=False)
