11743d45515d08d759ef8cc4ccc49f30d5d56d7f
VerdoodtDries <dries@nordend.eu>
2025-08-05 10:01:51 +0200

    Update, document and extend functie convert_ident8() with alphanumericlogic of an ident8 street segment.

--- utils/locatieservice_helpers.py
+++ utils/locatieservice_helpers.py
@@ -1,20 +1,29 @@
-# to do: implement function that converts a short ident8 (e.g. N8) to
+import re
+
 
 def convert_ident8(ident8: str, direction: str = 'P') -> str:
     """
     Converts a short notation of an ident8 road to a long notation of an ident8 road.
-    For example N8 > N0080001
+    For example: N8 > N0080001
+    If the road number is followed by a  letter [a-z], the direction starts with 901, etc...
+    a -> 901, b -> 902, etc...
+
     :param ident8:
     :param direction P (positive) or N (negative) direction
     :return: 8 character number
     """
     if direction not in ('N', 'P'):
         direction = 'P'
-        # raise ValueError('Parameter direction should be ''P'' (positive) or ''N'' (negative).')
+
     weg_letter = ident8[:1]
     weg_nummer = ident8[1:].rjust(3, '0')
-    if direction == 'P':
-        richting = '0001'
+
+    if match := re.search(
+        string=ident8, pattern='([a-zA-Z])$', flags=re.IGNORECASE
+    ):
+        ident8_suffix = match[1].lower()
+        richting_deel1 = 901 + (ord(ident8_suffix) - ord('a'))  # 'a' → 901, ..., 'z' → 926
     else:
-        richting = '0002'
-    return f'{weg_letter}{weg_nummer}{richting}'
+        richting_deel1 = '000'
+    richting_deel2 = '1' if direction == 'P' else '2'
+    return f'{weg_letter}{weg_nummer}{richting_deel1}{richting_deel2}'
