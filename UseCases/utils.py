4022d88a2a24cf0c181746ddc627cd21a7fc8d0b
VerdoodtDries <dries@nordend.eu>
2025-09-09 16:19:32 +0200

    new utils file

---
+++ UseCases/utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+
+def load_settings(user: str = 'Dries'):
+    if user == 'Dries':
+        return Path().home() / 'OneDrive - Nordend/projects/AWV/resources/settings_SyncOTLDataToLegacy.json'
+    else:
+        raise NotImplementedError(f'user: {user} is not implemented in function call load_settings()')
+
+def read_rsa_report(filepath: Path, usecols: list(str) =["uuid"]) -> pd.DataFrame:
+    """Read RSA-report as input into a DataFrame."""
+    return pd.read_excel(filepath, sheet_name='Resultaat', header=2, usecols=usecols)
+
