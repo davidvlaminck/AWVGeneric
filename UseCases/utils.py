27b9daac46ae6951b97e476578dcdb7ed09ca157
VerdoodtDries <dries@nordend.eu>
2025-09-16 07:08:11 +0200

    replace mutable default argument by None and initialize a list inside the function

--- UseCases/utils.py
+++ UseCases/utils.py
@@ -8,7 +8,9 @@ def load_settings(user: str = 'Dries'):
     else:
         raise NotImplementedError(f'user: {user} is not implemented in function call load_settings()')
 
-def read_rsa_report(filepath: Path, usecols: [str] =["uuid"]) -> pd.DataFrame:
+def read_rsa_report(filepath: Path, usecols: [str] = None) -> pd.DataFrame:
     """Read RSA-report as input into a DataFrame."""
+    if not usecols:
+        usecols = ["uuid"]
     return pd.read_excel(filepath, sheet_name='Resultaat', header=2, usecols=usecols)
 
