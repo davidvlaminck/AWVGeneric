530a18d713b4d5f3e75717c37641d7c37856f673
VerdoodtDries <dries@nordend.eu>
2025-09-26 15:19:41 +0200

    update script naar meer algemene benaderig voor een input file dat via DAVIE is gedownload.

--- UseCases/Eigenschap/locatie/set_afgeleide_locatie.py
+++ UseCases/Eigenschap/locatie/set_afgeleide_locatie.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 SETTINGS_PATH = Path.home() / 'OneDrive - Nordend/projects/AWV/resources/settings_SyncOTLDataToLegacy.json'
 INPUT_FILE = Path.home() / 'Downloads/SegmentController_Afstandsbewaking/Bevestigingsrelaties.xlsx'
-SHEETS = ['kast_ab', 'kast_ls', 'kast_lsdeel', 'lsdeel_segc']
+SHEETS = ['Bevestiging']
 
 def read_excel_as_dataframe(filepath: Path, sheet_name: str, usecols: list[str]):
     """Read RSA-report as input into a DataFrame."""
@@ -15,16 +15,17 @@ def read_excel_as_dataframe(filepath: Path, sheet_name: str, usecols: list[str])
 if __name__ == '__main__':
     logging.basicConfig(filename='logs.log', level=logging.DEBUG, format='%(levelname)s:\t%(asctime)s:\t%(message)s', filemode="w")
     logging.info('Updaten van de locatie-eigenschap van absoluut naar afgeleid.')
+    logging.info('Input file is op basis van een volledige DAVIE download van SegmentControllers en Afstandsbewaking, inclusief relaties.')
 
     eminfra_client = EMInfraClient(env=Environment.PRD, auth_type=AuthType.JWT, settings_path=SETTINGS_PATH)
 
     for sheet in SHEETS:
         df_assets = read_excel_as_dataframe(
-            filepath=INPUT_FILE, sheet_name=sheet, usecols=["bron_uuid", "bron_naam", "doel_uuid", "doel_naam"])
+            filepath=INPUT_FILE, sheet_name=sheet, usecols=["bronAssetId.identificator", "bron.typeURI", "doelAssetId.identificator", "doel.typeURI"])
 
         # Filter out assets with existing "afgeleide locaties"
-        df_assets = df_assets[df_assets.apply(lambda asset: eminfra_client.get_kenmerk_locatie_by_asset_uuid(asset_uuid=asset["bron_uuid"]).locatie is None, axis=1)]
+        df_assets = df_assets[df_assets.apply(lambda asset: eminfra_client.get_kenmerk_locatie_by_asset_uuid(asset_uuid=asset["bronAssetId.identificator"][:36]).locatie is None, axis=1)]
 
         for idx, asset in df_assets.iterrows():
-            logging.info(f'Locatie eigenschap updaten tussen bron-asset {asset["bron_uuid"]}: {asset["bron_naam"]} en doel-asset {asset["doel_uuid"]}: {asset["doel_naam"]}')
-            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset["bron_uuid"], doel_asset_uuid=asset["doel_uuid"])
+            logging.info(f'Locatie eigenschap updaten tussen bron-asset {asset["bronAssetId.identificator"][:36]}: {asset["bron.typeURI"]} en doel-asset {asset["doelAssetId.identificator"][:36]}: {asset["doel.typeURI"]}')
+            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset["bronAssetId.identificator"][:36], doel_asset_uuid=asset["doelAssetId.identificator"][:36])
