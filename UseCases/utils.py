9c3b1442627d494d2561c6fb07db6ffac9539ed0
VerdoodtDries <dries@nordend.eu>
2025-10-16 14:17:17 +0200

    Add function build_query_search_betrokkenerelaties()

--- UseCases/utils.py
+++ UseCases/utils.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from pathlib import Path
 
+from API.EMInfraDomain import AssetDTO, QueryDTO, PagingModeEnum, SelectionDTO, ExpressionDTO, TermDTO, OperatorEnum, \
+    AgentDTO, LogicalOpEnum
+
 
 def load_settings(user: str = 'Dries'):
     if user == 'Dries':
@@ -14,3 +17,26 @@ def read_rsa_report(filepath: Path, usecols: [str] = None) -> pd.DataFrame:
         usecols = ["uuid"]
     return pd.read_excel(filepath, sheet_name='Resultaat', header=2, usecols=usecols)
 
+def build_query_search_betrokkenerelaties(bronAsset: AssetDTO = None, agent: AgentDTO = None, rol: str = None) -> QueryDTO:
+    if all(par is None for par in (bronAsset, agent, rol)):
+        raise ValueError("At least one of the parameters 'bronAsset', 'agent', or 'rol' must be provided.")
+
+    expression_counter = 0
+    query = QueryDTO(size=5, from_=0, pagingMode=PagingModeEnum.OFFSET,
+                                 selection=SelectionDTO(expressions=[]))
+    if bronAsset:
+        logicalOp = None if expression_counter == 0 else LogicalOpEnum.AND
+        expression_counter += 1
+        expression_bronAsset = ExpressionDTO(terms=[TermDTO(property='bronAsset', operator=OperatorEnum.EQ, value=f'{bronAsset.uuid}')], logicalOp=logicalOp)
+        query.selection.expressions.append(expression_bronAsset)
+    if agent:
+        logicalOp = None if expression_counter == 0 else LogicalOpEnum.AND
+        expression_counter += 1
+        expression_agent = ExpressionDTO(terms=[TermDTO(property='agent', operator=OperatorEnum.EQ, value=f'{agent.uuid}')], logicalOp=logicalOp)
+        query.selection.expressions.append(expression_agent)
+    if rol:
+        logicalOp = None if expression_counter == 0 else LogicalOpEnum.AND
+        expression_counter += 1
+        expression_agent = ExpressionDTO(terms=[TermDTO(property='rol', operator=OperatorEnum.EQ, value=rol)], logicalOp=logicalOp)
+        query.selection.expressions.append(expression_agent)
+    return query
