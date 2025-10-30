0d060204872032b127d85b11639c362e4e3354fe
VerdoodtDries <dries@nordend.eu>
2025-10-30 09:31:12 +0100

    update template

--- UseCases/main_template.py
+++ UseCases/main_template.py
@@ -1,5 +1,7 @@
 import logging
 
+import pandas as pd
+
 from API.EMInfraClient import EMInfraClient
 from API.Enums import AuthType, Environment
 
@@ -13,5 +15,15 @@ if __name__ == '__main__':
 
     df_assets = read_rsa_report()
 
+    rows = []
     for _, asset in df_assets.iterrows():
         print("Implement function logic here")
+        row = {
+            "uuid": ''
+        }
+        rows.append(row)
+
+    output_excel_path = 'test_output.xlsx'
+    with pd.ExcelWriter(output_excel_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        df = pd.DataFrame(rows)
+        df.to_excel(writer, sheet_name='Sheet1', index=False, freeze_panes=[1, 1])
