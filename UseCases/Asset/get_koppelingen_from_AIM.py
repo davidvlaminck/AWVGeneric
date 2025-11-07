02b19d2fc59a83fa4ffe7e120ea2dcb23a134fba
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-11-07 16:14:38 +0100

    get koppelingen from AIM environment

---
+++ UseCases/Asset/get_koppelingen_from_AIM.py
@@ -0,0 +1,42 @@
+import json
+
+
+from API.EMInfraClient import EMInfraClient
+from API.EMInfraDomain import QueryDTO, PagingModeEnum, SelectionDTO, ExpressionDTO, TermDTO, OperatorEnum, \
+    LogicalOpEnum, ExpansionsDTO, BestekKoppeling, construct_naampad
+from API.Enums import AuthType, Environment
+
+
+if __name__ == '__main__':
+    from pathlib import Path
+
+    settings_path = Path('/home/davidlinux/Documenten/AWV/resources/settings_SyncOTLDataToLegacy.json')
+    eminfra_client = EMInfraClient(env=Environment.AIM, auth_type=AuthType.JWT, settings_path=settings_path)
+
+    query_dto = QueryDTO(size=100, from_=0, pagingMode=PagingModeEnum.OFFSET,
+                         expansions=ExpansionsDTO(fields=['parent']),
+                         selection=SelectionDTO(
+                             expressions=[ExpressionDTO(                                 terms=[
+                                        TermDTO(property='actief', operator=OperatorEnum.EQ,
+                                                value=True)
+                                     , TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
+                                               value='MAR.TUNNEL', logicalOp=LogicalOpEnum.AND)
+                                        ]
+                             )]))
+
+    generator_assets = eminfra_client.search_assets(query_dto=query_dto)
+
+    def to_serializable(bk: BestekKoppeling):
+        return json.loads(bk.json())
+
+    solution_list = []
+    for index, asset in enumerate(generator_assets):
+        print(index)
+        asset_d = {'asset_uuid': asset.uuid, 'naampad': construct_naampad(asset)}
+        bestekkoppelingen = eminfra_client.get_bestekkoppelingen_by_asset_uuid(asset_uuid=asset.uuid)
+        asset_d['bestekkoppelingen'] = [to_serializable(bk) for bk in bestekkoppelingen]
+        solution_list.append(asset_d)
+
+    # create a new file or overwrite
+    with open('mar.tunnel_koppelingen.json', 'w') as json_file:
+        json.dump(solution_list, json_file, indent=2)
