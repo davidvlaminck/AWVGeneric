17210ea4f1a4e494fc6ebaba0c7d762726b9f321
VerdoodtDries <dries@nordend.eu>
2025-11-19 10:55:12 +0100

    update template

--- UseCases/main_template.py
+++ UseCases/main_template.py
@@ -15,14 +15,21 @@ if __name__ == '__main__':
     df_assets = read_rsa_report()
 
     rows = []
-    for _, asset in df_assets.iterrows():
-        print("Implement function logic here")
+    for idx, df_row in df_assets.iterrows():
+        asset_uuid = df_row["id"]
+        logging.info(f"Processing asset: ({idx + 1}/{len(df_assets)}): asset_uuid: {asset_uuid}")
+        asset = eminfra_client.get_asset_by_id(asset_id=asset_uuid)
         row = {
             "uuid": ''
         }
         rows.append(row)
 
     output_excel_path = 'test_output.xlsx'
+    # Append to existing file
     with pd.ExcelWriter(output_excel_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        df = pd.DataFrame(rows)
+        df.to_excel(writer, sheet_name='Sheet1', index=False, freeze_panes=[1, 1])
+    # Write to a new file
+    with pd.ExcelWriter(output_excel_path, mode='w', engine='openpyxl') as writer:
         df = pd.DataFrame(rows)
         df.to_excel(writer, sheet_name='Sheet1', index=False, freeze_panes=[1, 1])
