e6d5197fc3eb603668aa4e9b2b47dd328cb719c6
VerdoodtDries <dries@nordend.eu>
2025-11-20 14:51:49 +0100

    work in progress

--- UseCases/DQ_Voeding/DQ_Voeding.py
+++ UseCases/DQ_Voeding/DQ_Voeding.py
@@ -13,16 +13,16 @@ ASSETTYPE_UUID_LS = '80fdf1b4-e311-4270-92ba-6367d2a42d47'
 ASSETTYPE_UUID_LSDEEL = 'b4361a72-e1d5-41c5-bfcc-d48f459f4048'
 ASSETTYPE_UUID_HS = '46dcd9b1-f660-4c8c-8e3e-9cf794b4de75'
 ASSETTYPE_UUID_HSDEEL = 'a9655f50-3de7-4c18-aa25-181c372486b1'
+ASSETTYPE_UUID_HSCABINELEGACY = '1cf24e76-5bf3-44b0-8332-a47ab126b87e'
 
-
-def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
+def add_relaties_vanuit_kast(client: EMInfraClient) -> [list]:
     """
-    Toevoegen van alle relaties vanuit een Kast.
+    Toevoegen van de explicitiete relaties vanuit een Kast op basis van de naampaden.
     Kast (Legacy): zoek de child-assets
-    LS (Legacy): zoek assets via de relatie HeeftBijhorendeAsset
+    LS (Legacy): zoek assets via het kenmerk HeeftBijhorendeAsset
 
     Opzoeken van alle bron assets (Kast).
-    Opzoeken van de doel assets (HS/HSDeel/LS/LSDeel).
+    Opzoeken van de doel assets (LS/LSDeel).
     Toevoegen van de bevestiging- en voedings-relaties
 
     HeeftBijhorendeAssets vanuit LS.
@@ -32,22 +32,24 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
     :param client:
     :return:
     """
-    query_search_kast = build_query_search_assettype(assettype_uuid=ASSETTYPE_UUID_KAST)
-    generator_kast = client.search_assets(query_dto=query_search_kast, actief=True)
-    for counter, asset in enumerate(generator_kast, start=1):
+    kast_meerdere_ls = []
+    kast_meerdere_lsdeel = []
+    ls_meerdere_dnb = []
+    ls_meerdere_energiemeterdnb = []
+    ls_meerdere_forfaitaireaansluiting = []
+
+    search_query = build_query_search_assettype(assettype_uuid=ASSETTYPE_UUID_KAST)
+    generator_asset = client.search_assets(query_dto=search_query, actief=True)
+    for counter, asset in enumerate(generator_asset, start=1):
         logging.info(f'Processing ({counter}) asset: {asset.uuid}')
 
         locatie_kenmerk = eminfra_client.get_kenmerk_locatie_by_asset_uuid(asset_uuid=asset.uuid)
 
-        child_assets_van_kast = list(client.search_child_assets(asset_uuid=asset.uuid, recursive=False))
-        assets_ls = [item for item in child_assets_van_kast if
+        child_assets = list(client.search_child_assets(asset_uuid=asset.uuid, recursive=False))
+        assets_ls = [item for item in child_assets if
                      item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#LS']
-        assets_lsdeel = [item for item in child_assets_van_kast if
+        assets_lsdeel = [item for item in child_assets if
                          item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#LSDeel']
-        assets_hs = [item for item in child_assets_van_kast if
-                     item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#HS']
-        assets_hsdeel = [item for item in child_assets_van_kast if
-                         item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#HSDeel']
 
         asset_ls = None
         if len(assets_ls) == 1:
@@ -57,7 +59,7 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
                                       relatie=RelatieEnum.BEVESTIGING)
             eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_ls.uuid, doel_asset_uuid=asset.uuid)
         elif len(assets_ls) > 1:
-            assets_meerdere_doelen.append(asset)
+            kast_meerdere_ls.append(asset)
 
         asset_lsdeel = None
         if len(assets_lsdeel) == 1:
@@ -68,50 +70,23 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
             eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_lsdeel.uuid,
                                                               doel_asset_uuid=asset.uuid)
         elif len(assets_lsdeel) > 1:
-            assets_meerdere_doelen.append(asset)
+            kast_meerdere_lsdeel.append(asset)
 
         if asset_ls and asset_lsdeel:
             logging.debug("Voedt-relatie van LS naar LSDeel")
             create_relatie_if_missing(client=client, bron_asset=asset_ls, doel_asset=asset_lsdeel,
                                       relatie=RelatieEnum.VOEDT)
 
-        asset_hs = None
-        if len(assets_hs) == 1:
-            logging.debug("Bevestiging-relatie van Kast naar HS")
-            asset_hs = assets_hs[0]
-            create_relatie_if_missing(client=client, bron_asset=asset, doel_asset=asset_hs,
-                                      relatie=RelatieEnum.BEVESTIGING)
-            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_hs.uuid, doel_asset_uuid=asset.uuid)
-        elif len(assets_hs) > 1:
-            assets_meerdere_doelen.append(asset)
+        if not asset_ls:
+            continue
 
-        asset_hsdeel = None
-        if len(assets_hsdeel) == 1:
-            logging.debug("Bevestiging-relatie van Kast naar HSDeel")
-            asset_hsdeel = assets_hsdeel[0]
-            create_relatie_if_missing(client=client, bron_asset=asset, doel_asset=asset_hsdeel,
-                                      relatie=RelatieEnum.BEVESTIGING)
-            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_hsdeel.uuid,
-                                                              doel_asset_uuid=asset.uuid)
-        elif len(assets_hsdeel) > 1:
-            assets_meerdere_doelen.append(asset)
-
-        if asset_hs and asset_hsdeel:
-            logging.debug("Voedt-relatie van HS naar HSDeel")
-            create_relatie_if_missing(client=client, bron_asset=asset_hs, doel_asset=asset_hsdeel,
-                                      relatie=RelatieEnum.VOEDT)
-        if asset_hsdeel and asset_lsdeel:
-            logging.debug("Voedt-relatie van HSDeel naar LSDeel")
-            create_relatie_if_missing(client=client, bron_asset=asset_hsdeel, doel_asset=asset_lsdeel,
-                                      relatie=RelatieEnum.VOEDT)
-
-        heeftbijhorende_assets_van_ls = client.search_assets_via_relatie(asset_uuid=asset_ls.uuid,
-                                                                         relatie=RelatieEnum.HEEFTBIJHORENDEASSETS)
-        assets_dnblaagspanning = [item for item in heeftbijhorende_assets_van_ls if
+        heeftbijhorende_assets = client.search_assets_via_relatie(asset_uuid=asset_ls.uuid,
+                                                                  relatie=RelatieEnum.HEEFTBIJHORENDEASSETS)
+        assets_dnblaagspanning = [item for item in heeftbijhorende_assets if
                                   item.type.uri == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#DNBLaagspanning']
-        assets_energiemeterdnb = [item for item in heeftbijhorende_assets_van_ls if
+        assets_energiemeterdnb = [item for item in heeftbijhorende_assets if
                                   item.type.uri == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#EnergiemeterDNB']
-        assets_forfaitaireaansluiting = [item for item in heeftbijhorende_assets_van_ls if
+        assets_forfaitaireaansluiting = [item for item in heeftbijhorende_assets if
                                          item.type.uri == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#ForfaitaireAansluiting']
 
         asset_dnblaagspanning = None
@@ -122,7 +97,7 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
                                       relatie=RelatieEnum.HOORTBIJ)
             logging.debug("DNBLaagspanning heeft geen eigenschap locatie")
         elif len(assets_dnblaagspanning) > 1:
-            assets_meerdere_doelen.append(asset_ls)
+            ls_meerdere_dnb.append(asset_ls)
 
         asset_energiemeterdnb = None
         if len(assets_energiemeterdnb) == 1:
@@ -134,7 +109,7 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
             eminfra_client.update_geometrie_by_asset_uuid(asset_uuid=asset_energiemeterdnb.uuid,
                                                           wkt_geometry=wkt_geometry)
         elif len(assets_energiemeterdnb) > 1:
-            assets_meerdere_doelen.append(asset_ls)
+            ls_meerdere_energiemeterdnb.append(asset_ls)
 
         asset_forfaitaireaansluiting = None
         if len(assets_forfaitaireaansluiting) == 1:
@@ -145,12 +120,12 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
             eminfra_client.update_kenmerk_locatie_by_asset_uuid(asset_uuid=asset.uuid,
                                                                 wkt_geom=locatie_kenmerk.geometrie)
         elif len(assets_forfaitaireaansluiting) > 1:
-            assets_meerdere_doelen.append(asset_ls)
+            ls_meerdere_forfaitaireaansluiting.append(asset_ls)
 
         if asset_dnblaagspanning and asset_energiemeterdnb:
             logging.debug("Voedt-relatie van DNBLaagspanning naar EnergiemeterDNB")
-            create_relatie_if_missing(client=client, bron_asset=asset_dnblaagspanning, doel_asset=asset_energiemeterdnb,
-                                      relatie=RelatieEnum.VOEDT)
+            create_relatie_if_missing(client=client, bron_asset=asset_dnblaagspanning,
+                                      doel_asset=asset_energiemeterdnb, relatie=RelatieEnum.VOEDT)
         if asset_dnblaagspanning and asset_forfaitaireaansluiting:
             logging.debug("Voedt-relatie van DNBLaagspanning naar ForfaitaireAansluiting")
             create_relatie_if_missing(client=client, bron_asset=asset_dnblaagspanning,
@@ -159,11 +134,134 @@ def add_relaties_vanuit_kast(client: EMInfraClient) -> None:
         if counter % 5 == 0:
             break
 
+    return [kast_meerdere_ls, kast_meerdere_lsdeel,
+            ls_meerdere_dnb, ls_meerdere_energiemeterdnb, ls_meerdere_forfaitaireaansluiting]
+
+def add_relaties_vanuit_hscabine(client: EMInfraClient) -> [list]:
+    """
+    Toevoegen van de expliciete relaties vanuit een HSCabine (Legacy) op basis van de boomstructuur.
+    HSCabine (Legacy): zoek de child-assets
+
+    Opzoeken van alle bron assets (HSCabine (Legacy)).
+    Opzoeken van de doel assets (HS/HSDeel/LSDeel).
+    Toevoegen van de bevestiging- en voedings-relaties
+
+    :param client:
+    :return:
+    """
+    hscab_meerdere_hs = []
+    hscab_meerdere_hsdeel = []
+    hscab_meerdere_lsdeel = []
+    hs_meerdere_dnbhoogspanning = []
+    hs_meerdere_energiemeter= []
+
+    search_query = build_query_search_assettype(assettype_uuid=ASSETTYPE_UUID_HSCABINELEGACY)
+    generator_asset = client.search_assets(query_dto=search_query, actief=True)
+    for counter, asset in enumerate(generator_asset, start=1):
+        logging.info(f'Processing ({counter}) asset: {asset.uuid}')
+
+        locatie_kenmerk = eminfra_client.get_kenmerk_locatie_by_asset_uuid(asset_uuid=asset.uuid)
+
+        child_assets = list(client.search_child_assets(asset_uuid=asset.uuid, recursive=False))
+        assets_hs = [item for item in child_assets if
+                     item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#HS']
+        assets_hsdeel = [item for item in child_assets if
+                         item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#HSDeel']
+        assets_lsdeel = [item for item in child_assets if
+                         item.type.uri == 'https://lgc.data.wegenenverkeer.be/ns/installatie#LSDeel']
+
+        asset_hs = None
+        if len(assets_hs) == 1:
+            logging.debug("Bevestiging-relatie van HSCabineLegacy naar HS")
+            asset_hs = assets_hs[0]
+            create_relatie_if_missing(client=client, bron_asset=asset, doel_asset=asset_hs,
+                                      relatie=RelatieEnum.BEVESTIGING)
+            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_hs.uuid, doel_asset_uuid=asset.uuid)
+        elif len(assets_hs) > 1:
+            hscab_meerdere_hs.append(asset)
+
+        asset_hsdeel = None
+        if len(assets_hsdeel) == 1:
+            logging.debug("Bevestiging-relatie van HSCabineLegacy naar HSDeel")
+            asset_hsdeel = assets_hsdeel[0]
+            create_relatie_if_missing(client=client, bron_asset=asset, doel_asset=asset_hsdeel,
+                                      relatie=RelatieEnum.BEVESTIGING)
+            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_hsdeel.uuid,
+                                                              doel_asset_uuid=asset.uuid)
+        elif len(assets_hsdeel) > 1:
+            hscab_meerdere_hsdeel.append(asset)
+
+        asset_lsdeel = None
+        if len(assets_lsdeel) == 1:
+            logging.debug("Bevestiging-relatie van HSCabineLegacy naar LSDeel")
+            asset_lsdeel = assets_lsdeel[0]
+            create_relatie_if_missing(client=client, bron_asset=asset, doel_asset=asset_lsdeel,
+                                      relatie=RelatieEnum.BEVESTIGING)
+            eminfra_client.update_kenmerk_locatie_via_relatie(bron_asset_uuid=asset_lsdeel.uuid,
+                                                              doel_asset_uuid=asset.uuid)
+        elif len(assets_lsdeel) > 1:
+            hscab_meerdere_lsdeel.append(asset)
+
+        if asset_hs and asset_hsdeel:
+            logging.debug("Voedt-relatie van HS naar HSDeel")
+            create_relatie_if_missing(client=client, bron_asset=asset_hs, doel_asset=asset_hsdeel,
+                                      relatie=RelatieEnum.VOEDT)
+
+        if asset_hsdeel and asset_lsdeel:
+            logging.debug("Voedt-relatie van HSDeel naar LSDeel")
+            create_relatie_if_missing(client=client, bron_asset=asset_hsdeel, doel_asset=asset_lsdeel,
+                                      relatie=RelatieEnum.VOEDT)
+
+        if not asset_hs:
+            continue
+
+        heeftbijhorende_assets = client.search_assets_via_relatie(asset_uuid=asset_hs.uuid,
+                                                                  relatie=RelatieEnum.HEEFTBIJHORENDEASSETS)
+        assets_dnbhoogspanning = [item for item in heeftbijhorende_assets if
+                                  item.type.uri == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#DNBHoogspanning']
+        assets_energiemeterdnb = [item for item in heeftbijhorende_assets if
+                                  item.type.uri == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#EnergiemeterDNB']
+
+        asset_dnbhoogspanning = None
+        if len(assets_dnbhoogspanning) == 1:
+            logging.debug("HeeftBijhorendeAssets-relatie van LS naar DNBLaagspanning")
+            asset_dnbhoogspanning = assets_dnbhoogspanning[0]
+            create_relatie_if_missing(client=client, bron_asset=asset_dnbhoogspanning, doel_asset=asset_hs,
+                                      relatie=RelatieEnum.HOORTBIJ)
+            logging.debug("DNBLaagspanning heeft geen eigenschap locatie")
+        elif len(assets_dnbhoogspanning) > 1:
+            hs_meerdere_dnbhoogspanning.append(asset_hs)
+
+        asset_energiemeterdnb = None
+        if len(assets_energiemeterdnb) == 1:
+            logging.debug("HeeftBijhorendeAssets-relatie van LS naar EnergiemeterDNB")
+            asset_energiemeterdnb = assets_energiemeterdnb[0]
+            create_relatie_if_missing(client=client, bron_asset=asset_energiemeterdnb, doel_asset=asset_hs,
+                                      relatie=RelatieEnum.HOORTBIJ)
+            wkt_geometry = format_locatie_kenmerk_lgc_2_wkt(locatie=locatie_kenmerk)
+            eminfra_client.update_geometrie_by_asset_uuid(asset_uuid=asset_energiemeterdnb.uuid,
+                                                          wkt_geometry=wkt_geometry)
+        elif len(assets_energiemeterdnb) > 1:
+            hs_meerdere_energiemeter.append(asset_hs)
+
+        if counter % 5 == 0:
+            break
+
+    return [hscab_meerdere_hs, hscab_meerdere_hsdeel, hscab_meerdere_lsdeel, hs_meerdere_dnbhoogspanning,
+            hs_meerdere_energiemeter]
+
 
 if __name__ == '__main__':
     configure_logger()
     logging.info('Kwaliteitscontrole van voeding-gerelateerde assets.')
-    eminfra_client = EMInfraClient(env=Environment.TEI, auth_type=AuthType.JWT, settings_path=load_settings())
+    eminfra_client = EMInfraClient(env=Environment.DEV, auth_type=AuthType.JWT, settings_path=load_settings())
 
-    global assets_meerdere_doelen
-    add_relaties_vanuit_kast(client=eminfra_client)
+    [kast_meerdere_ls, kast_meerdere_lsdeel, ls_meerdere_dnb, ls_meerdere_energiemeterdnb,
+     ls_meerdere_forfaitaireaansluiting] = add_relaties_vanuit_kast(client=eminfra_client)
+
+    [hscab_meerdere_hs, hscab_meerdere_hsdeel, hscab_meerdere_lsdeel, hs_meerdere_dnbhoogspanning,
+            hs_meerdere_energiemeter] = add_relaties_vanuit_hscabine(client=eminfra_client)
+
+    # todo: bestaande en nieuwe geometrie vergelijken.
+    # Pas niets aan indien de locatie reeds is afgeleid
+    # Geef een warning als de afstand te groot is.
