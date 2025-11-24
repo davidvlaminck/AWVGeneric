2a30d9653e76ad9afe40933776995200fde10a16
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-11-24 20:42:55 +0100

    minor correction

--- UseCases/Asset/main_search_lgc_types.py
+++ UseCases/Asset/main_search_lgc_types.py
@@ -5,7 +5,7 @@ from API.Enums import AuthType, Environment
 if __name__ == '__main__':
     from pathlib import Path
 
-    settings_path = Path('/home/davidlinux/Documents/AWV/resources/settings_SyncOTLDataToLegacy.json')
+    settings_path = Path('/home/davidlinux/Documenten/AWV/resources/settings_SyncOTLDataToLegacy.json')
     eminfra_client = EMInfraClient(env=Environment.PRD, auth_type=AuthType.JWT, settings_path=settings_path)
 
     asset_types = list(eminfra_client.get_all_legacy_assettypes())
