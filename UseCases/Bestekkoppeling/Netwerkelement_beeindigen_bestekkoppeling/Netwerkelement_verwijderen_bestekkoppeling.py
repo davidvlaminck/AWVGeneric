127e5e21e335475b0178bee8ad2883ec6975914c
VerdoodtDries <dries@nordend.eu>
2025-11-25 14:12:46 +0100

    initieel script

---
+++ UseCases/Bestekkoppeling/Netwerkelement_beeindigen_bestekkoppeling/Netwerkelement_verwijderen_bestekkoppeling.py
@@ -0,0 +1,53 @@
+import logging
+from datetime import datetime
+
+import pandas as pd
+
+from API.EMInfraClient import EMInfraClient
+from API.EMInfraDomain import BestekCategorieEnum, BestekKoppelingStatusEnum
+from API.Enums import AuthType, Environment
+from UseCases.utils import configure_logger, load_settings
+from utils.query_dto_helpers import build_query_search_assettype
+
+ASSETTYPE_UUID_NETWERKELEMENT = 'b6f86b8d-543d-4525-8458-36b498333416'
+SPECIFIC_DATE = datetime(year=2025, month=11, day=21).date()
+OUTPUT_EXCEL_PATH = 'Netwerkelementen_Swarco_VWT-NET-2020-017_verwijderen.xlsx'
+
+
+if __name__ == '__main__':
+    configure_logger()
+    logging.info('Verwijderen van inactieve bestekkoppeling "VWT/NET/2020/017".')
+    eminfra_client = EMInfraClient(env=Environment.PRD, auth_type=AuthType.JWT, settings_path=load_settings())
+
+    logging.info("Get all assets of assettype Netwerkelement.")
+    search_query = build_query_search_assettype(assettype_uuid=ASSETTYPE_UUID_NETWERKELEMENT)
+    generator = eminfra_client.search_assets(query_dto=search_query, actief=True)
+
+    assets_updated = []
+    for counter, asset in enumerate(generator, start=1):
+        logging.debug(f'Processing ({counter}) asset: {asset.uuid}; naam: {asset.naam}; assettype: {asset.type.uri}')
+
+        bestekkoppelingen = eminfra_client.get_bestekkoppelingen_by_asset_uuid(asset_uuid=asset.uuid)
+        bestekRef_swarco_2020_17 = eminfra_client.get_bestekref_by_eDelta_dossiernummer(eDelta_dossiernummer='VWT/NET/2020/017')
+        index, matching_bestekkoppeling = next(((i, x) for i, x in enumerate(bestekkoppelingen) if x.status == BestekKoppelingStatusEnum.INACTIEF and x.bestekRef.uuid == bestekRef_swarco_2020_17.uuid and x.categorie == BestekCategorieEnum.WERKBESTEK), (None, None))
+
+        if matching_bestekkoppeling and datetime.fromisoformat(matching_bestekkoppeling.startDatum).date() == SPECIFIC_DATE:
+            logging.debug(f'Verwijder bestekkoppeling {matching_bestekkoppeling.bestekRef.eDeltaBesteknummer}')
+            assets_updated.append({
+                "uuid": asset.uuid
+                , "naam": asset.naam
+                , "bestek": 'VWT/NET/2020/017'
+                , "Start koppeling": matching_bestekkoppeling.startDatum
+                , "Einde koppeling": matching_bestekkoppeling.eindDatum
+                , "eminfra_link": f'https://apps.mow.vlaanderen.be/eminfra/assets/{asset.uuid}'
+            })
+            del bestekkoppelingen[index]
+            eminfra_client.change_bestekkoppelingen_by_asset_uuid(asset.uuid, bestekkoppelingen)
+        else:
+            logging.debug('Ga verder met het volgende Netwerkelement,'
+                          'geen overeenkomstige bestekkoppeling of minstens 2 actieve werkbestekken.')
+            continue
+
+    with pd.ExcelWriter(OUTPUT_EXCEL_PATH, mode='w', engine='openpyxl') as writer:
+        df = pd.DataFrame(assets_updated)
+        df.to_excel(writer, sheet_name='Sheet1', index=False, freeze_panes=[1, 1])
