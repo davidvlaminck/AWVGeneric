69a4475cc9cfc021a8ff15c5aa09d1d4a51e9174
VerdoodtDries <dries@nordend.eu>
2026-03-12 15:53:52 +0100

    update work in progress

--- UseCases/Asset/main_search_assets.py
+++ UseCases/Asset/main_search_assets.py
@@ -4,33 +4,39 @@ from API.eminfra.EMInfraClient import EMInfraClient
 from API.eminfra.EMInfraDomain import QueryDTO, PagingModeEnum, SelectionDTO, ExpressionDTO, TermDTO, OperatorEnum, \
     LogicalOpEnum, ExpansionsDTO, construct_naampad
 from API.Enums import AuthType, Environment
+from UseCases.utils import load_settings_path
 
-# requires prettytable, requests, pyjwt
+ENVIRONMENT = Environment.PRD
 
 if __name__ == '__main__':
-    from pathlib import Path
+    settings_path = load_settings_path()
+    eminfra_client = EMInfraClient(env=ENVIRONMENT, auth_type=AuthType.JWT, settings_path=settings_path)
 
-    settings_path = Path('/home/davidlinux/Documents/AWV/resources/settings_SyncOTLDataToLegacy.json')
-    eminfra_client = EMInfraClient(env=Environment.PRD, auth_type=AuthType.JWT, settings_path=settings_path)
-
-    asset_types = list(eminfra_client.get_all_otl_assettypes())
+    asset_types = list(eminfra_client.assettype_service.get_all_assettypes())
     print(f'aantal OTL types: {len(asset_types)}')
 
     type_term = TermDTO(property='type', operator=OperatorEnum.EQ, value='a7eadedf-b5cf-491b-8b89-ccced9a37004')
+    headers = ['uuid', 'type', 'naampad', 'em_infra_link']
     query_dto = QueryDTO(size=100, from_=0, pagingMode=PagingModeEnum.OFFSET,
                          expansions=ExpansionsDTO(fields=['parent']),
                          selection=SelectionDTO(
                              expressions=[ExpressionDTO(
-                                 terms=[type_term,
-                                     TermDTO(property='actief', operator=OperatorEnum.EQ,
-                                             value=True, logicalOp=LogicalOpEnum.AND),
-                                     TermDTO(property='beheerobject', operator=OperatorEnum.EQ,
-                                             value=None, logicalOp=LogicalOpEnum.AND, negate=True),
-                                     TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
-                                             value='DA-', logicalOp=LogicalOpEnum.AND, negate=True),
-                                     TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
-                                             value='OTN.', logicalOp=LogicalOpEnum.AND, negate=True)])]))
-    headers = ['uuid', 'type', 'naampad', 'em_infra_link']
+                                 terms=[
+                                     # type_term
+                                     # ,
+                                     TermDTO(property='"actiefOfToekomstigBestek"', operator=OperatorEnum.EQ,
+                                             value='09125be9-febd-471d-bec4-ae57ef3e5800')
+                                     , TermDTO(property='actief', operator=OperatorEnum.EQ,
+                                             value=True, logicalOp=LogicalOpEnum.AND)
+                                     # , TermDTO(property='beheerobject', operator=OperatorEnum.EQ,
+                                     #         value=None, logicalOp=LogicalOpEnum.AND, negate=True)
+                                     # , TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
+                                     #         value='DA-', logicalOp=LogicalOpEnum.AND, negate=True)
+                                     # , TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
+                                     #         value='OTN.', logicalOp=LogicalOpEnum.AND, negate=True)
+                                 ])
+                             ])
+                         )
     rows = []
     for otl_asset_type in asset_types:
         print(f'querying type {otl_asset_type.korteUri}')
@@ -38,7 +44,7 @@ if __name__ == '__main__':
         query_dto.from_ = 0
         rows.extend([asset.uuid, otl_asset_type.korteUri, construct_naampad(asset),
                      f'https://apps.mow.vlaanderen.be/eminfra/assets/{asset.uuid}']
-                    for asset in eminfra_client.search_assets(query_dto))
+                    for asset in eminfra_client.asset_service.search_assets_generator(query_dto))
     table = PrettyTable(headers)
     table.add_rows(rows)
 
