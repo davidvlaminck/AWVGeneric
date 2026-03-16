5a42f016bd78735b323aa864bdb5a842202d121a
VerdoodtDries <dries@nordend.eu>
2026-03-16 10:39:53 +0100

    update work in progress

--- UseCases/Asset/main_search_assets.py
+++ UseCases/Asset/main_search_assets.py
@@ -4,6 +4,7 @@ from API.eminfra.EMInfraClient import EMInfraClient
 from API.eminfra.EMInfraDomain import QueryDTO, PagingModeEnum, SelectionDTO, ExpressionDTO, TermDTO, OperatorEnum, \
     LogicalOpEnum, ExpansionsDTO, construct_naampad
 from API.Enums import AuthType, Environment
+from UseCases.Agents.get_agents import query_dto
 from UseCases.utils import load_settings_path
 
 ENVIRONMENT = Environment.PRD
@@ -15,32 +16,42 @@ if __name__ == '__main__':
     asset_types = list(eminfra_client.assettype_service.get_all_assettypes())
     print(f'aantal OTL types: {len(asset_types)}')
 
-    type_term = TermDTO(property='type', operator=OperatorEnum.EQ, value='a7eadedf-b5cf-491b-8b89-ccced9a37004')
-    headers = ['uuid', 'type', 'naampad', 'em_infra_link']
+    # todo deze logica voor het opbiouwen van een QueryDTO zoekterm naar een utils functie vertalen OF naar de module AssetService verplaatsen
+    term_type = TermDTO(property='type', operator=OperatorEnum.EQ, value='a7eadedf-b5cf-491b-8b89-ccced9a37004')
+    term_bestek = TermDTO(property='"actiefOfToekomstigBestek"', operator=OperatorEnum.EQ, value='09125be9-febd-471d-bec4-ae57ef3e5800') # INTERN-099
+    term_actief = TermDTO(property='actief', operator=OperatorEnum.EQ, value=True, logicalOp=LogicalOpEnum.AND)
+
+    terms = []
+    first_term = True
+    if term_type:
+        if not first_term:
+            term_type.append(logicalOp=LogicalOpEnum.AND)
+            first_term = False
+        terms.append(term_type)
+    if term_bestek:
+        if not first_term:
+            term_bestek.append(logicalOp=LogicalOpEnum.AND)
+            first_term = False
+        terms.append(term_bestek)
+    if term_actief:
+        if not first_term:
+            term_actief.append(logicalOp=LogicalOpEnum.AND)
+            first_term = False
+        terms.append(term_actief)
+
     query_dto = QueryDTO(size=100, from_=0, pagingMode=PagingModeEnum.OFFSET,
                          expansions=ExpansionsDTO(fields=['parent']),
                          selection=SelectionDTO(
                              expressions=[ExpressionDTO(
-                                 terms=[
-                                     # type_term
-                                     # ,
-                                     TermDTO(property='"actiefOfToekomstigBestek"', operator=OperatorEnum.EQ,
-                                             value='09125be9-febd-471d-bec4-ae57ef3e5800')
-                                     , TermDTO(property='actief', operator=OperatorEnum.EQ,
-                                             value=True, logicalOp=LogicalOpEnum.AND)
-                                     # , TermDTO(property='beheerobject', operator=OperatorEnum.EQ,
-                                     #         value=None, logicalOp=LogicalOpEnum.AND, negate=True)
-                                     # , TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
-                                     #         value='DA-', logicalOp=LogicalOpEnum.AND, negate=True)
-                                     # , TermDTO(property='naamPad', operator=OperatorEnum.STARTS_WITH,
-                                     #         value='OTN.', logicalOp=LogicalOpEnum.AND, negate=True)
-                                 ])
+                                 terms=[terms])
                              ])
                          )
+
+    headers = ['uuid', 'type', 'naampad', 'em_infra_link']
     rows = []
     for otl_asset_type in asset_types:
         print(f'querying type {otl_asset_type.korteUri}')
-        type_term.value = otl_asset_type.uuid
+        term_type.value = otl_asset_type.uuid
         query_dto.from_ = 0
         rows.extend([asset.uuid, otl_asset_type.korteUri, construct_naampad(asset),
                      f'https://apps.mow.vlaanderen.be/eminfra/assets/{asset.uuid}']
